57113ee9167aa30b8bf272d2e5dfcc005ddf8545
Rizki Madya <rizkimadya88@gmail.com>
2023-05-18 12:31:21 +0800

    crud api laravel 10

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -7,13 +7,23 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class PostResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
+    public $status;
+    public $message;
+    public $resource;
+
+    public function __construct($status, $message, $resource)
+    {
+        parent::__construct($resource);
+        $this->status = $status;
+        $this->message = $message;
+    }
+
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'success' => $this->status,
+            'message' => $this->message,
+            'data' => $this->resource,
+        ];
     }
 }
